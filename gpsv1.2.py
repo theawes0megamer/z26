79812edebd40ef48fee4163e278902e376041eb1
theawes0megamer <altonito@icloud.com>
2023-12-21 16:18:37 -0700

    fixes

--- gpsv1.2.py
+++ gpsv1.2.py
@@ -1,20 +1,16 @@
 import serial
-import pynmea2
+import pyubx2
 
 # Replace '/dev/ttyUSB0' with the correct serial port for your GPS device
 serial_port = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+ubr = pyubx2.UBXReader(serial_port)
 
-def get_gps_data():
-    try:
-        raw_data = serial_port.readline().decode('utf-8')
-        if raw_data.startswith('$GPGGA'):  # Example NMEA sentence, adjust based on your GPS device
-            msg = pynmea2.parse(raw_data)
-            return msg
-    except serial.SerialException:
-        pass
-
-# Example usage:
 while True:
-    data = get_gps_data()
-    if data:
-        print(f" \r Latitude: {data.latitude}, Longitude: {data.longitude}, Speed: {data.spd_over_grnd}")
+    try:
+        raw_data, parsed_data = ubr.read()
+        if parsed_data and hasattr(parsed_data, 'ground_speed'):  # Adjust the attribute name
+            gspeed = parsed_data.ground_speed  # Adjust the attribute name
+            mph = gspeed * 1.150779
+            print(f"Ground Speed: {mph} mph")
+    except (serial.SerialException, pyubx2.UBXStreamError):
+        pass
