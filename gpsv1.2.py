4c0b61b8c6eadbf1d25fa52118fb581c1097dcb2
theawes0megamer <98134376+theawes0megamer@users.noreply.github.com>
2023-12-28 12:20:13 -0700

    Update gpsv1.2.py

--- gpsv1.2.py
+++ gpsv1.2.py
@@ -1,14 +1,4 @@
-from serial import Serial
 from pyubx2 import UBXReader
-
-try:
-    stream = Serial('COM8', 38400)
-    while True:
-        ubr = UBXReader(stream)
-        (raw_data, parsed_data) = ubr.read()
-        # print(parsed_data)
-        if parsed_data.identity == "NAV-PVT":
-            lat, lon, alt = parsed_data.lat, parsed_data.lon, parsed_data.hMSL
-            print(f"lat = {lat}, lon = {lon}, alt = {alt/1000} m")
-except KeyboardInterrupt:
-    print("Terminated by user")
+stream = Serial('/dev/ttyS0', 115200, timeout=3)
+ubr = UBXReader(stream, protfilter=2)
+for (raw_data, parsed_data) in ubr: print(parsed_data)
