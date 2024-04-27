ab2685a6bcdabd6c9cd19eaf7117e65efc66b48a
theawes0megamer <altonito@icloud.com>
2024-04-27 11:53:12 -0600

    fix START_TIME var

--- main_gpsd.py
+++ main_gpsd.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import time
-import serial
 import gps
 
 
@@ -74,6 +73,7 @@ def update_time():  # Update the time in the UI
 
 def update_mph():
     global mph
+    global start_time
     try:
         while 0 == session.read():
             if not (gps.MODE_SET & session.valid):
@@ -99,9 +99,6 @@ def update_mph():
         
 
 def start_timer():  # Start the 0-60 MPH timer
-    global start_time
-    start_time = None
-
     if mph > 1 and not start_time:
         start_time = time.time()
         update_timer()
